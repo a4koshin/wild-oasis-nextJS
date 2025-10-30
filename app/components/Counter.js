d8de8cf76bdf548d5352ea9b4244f16af883c7f7
a4kooshinn <mankajr11@gmail.com>
2025-10-30 21:20:27 +0300

    adding the interactivity

---
+++ app/components/Counter.js
@@ -0,0 +1,8 @@
+"use client";
+
+import { useState } from "react";
+export default function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  return <button onClick={() => setCounter((c) => c + 1)}>{counter}</button>;
+}
