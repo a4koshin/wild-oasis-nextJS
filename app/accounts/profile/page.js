926bdb70df3ff48e3e89a90dbb4437b4d6e9d620
a4kooshinn <mankajr11@gmail.com>
2025-10-31 19:18:40 +0300

    Added Nested Layout to the Page

--- app/accounts/profile/page.js
+++ app/accounts/profile/page.js
@@ -1,4 +1,8 @@
 import SelectCountry from "@/app/_components/SelectCountry";
+import Image from "next/image";
+export const metadata = {
+  title: "Profile",
+};
 
 export default function Page() {
   // CHANGE
@@ -36,8 +40,10 @@ export default function Page() {
         <div className="space-y-2">
           <div className="flex items-center justify-between">
             <label htmlFor="nationality">Where are you from?</label>
-            <img
-              src={countryFlag}
+            <Image
+              src={`/${countryFlag}`}
+              width={20}
+              height={20}
               alt="Country flag"
               className="h-5 rounded-sm"
             />
